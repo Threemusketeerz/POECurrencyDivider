e9cc4972970428d795d0e4548edda42c59594b09
Bjarke Sporring (floppydisc) <work.bjarke@gmail.com>
2018-09-10 07:33:01 +0200

    Considerate to different status codes. Updated last entry url visited

--- divider.py
+++ divider.py
@@ -31,32 +31,35 @@ def find_tab():
         response = requests.get(api_url + id_url)
         print(f"Status: {response.status_code}")
 
-        # Write last visited ID to a file
-        with open("last_visited_id.txt", "w") as out:
-            out.write(f"{id}")
+        # If response is OK
+        # Else it should keep trying
+        if response.status_code is 200:
+            # Write last visited ID to a file
+            with open("last_visited_id.txt", "w") as out:
+                out.write(f"{id}")
 
-        response_json = response.json()
+            response_json = response.json()
 
-        stashes = response_json.get("stashes")
+            stashes = response_json.get("stashes")
 
-        if stashes:
-            for stash in stashes:
-                account_name = stash.get("accountName")
-                items = stash.get("items")
-                if account_name:
-                    print(f"Looking in: {account_name}")
-                if (account_name == account):
-                    print("FOUND sporring2")
+            if stashes:
+                for stash in stashes:
+                    account_name = stash.get("accountName")
+                    items = stash.get("items")
+                    if account_name:
+                        print(f"Looking in: {account_name}")
+                    if (account_name == account):
+                        print("FOUND sporring2")
 
-                    with open("found_at_id.txt", "w") as out:
-                        print("Writing location to a file")
-                        out.write(f"{id}")
+                        with open("found_at_id.txt", "w") as out:
+                            print("Writing location to a file")
+                            out.write(f"{id}")
 
-                    found = True
+                        found = True
 
-        if not found:
-            id = response_json.get("next_change_id")
-            # print(f"Didn't find {account}")
+            if not found:
+                id = response_json.get("next_change_id")
+                # print(f"Didn't find {account}")
     # Did we succeed
     print(f"Response: {response.status_code}")
 
