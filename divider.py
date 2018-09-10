e3404ee19dd18d7bd463de75db437d4aec235b7f
Bjarke Sporring (floppydisc) <work.bjarke@gmail.com>
2018-09-10 05:49:30 +0200

    Posistion in look is now persistent. Gonna be saving this 'till my user has been found

--- divider.py
+++ divider.py
@@ -1,3 +1,4 @@
+import os
 import json
 import requests
 
@@ -12,16 +13,27 @@ def find_tab():
 
     found = False
 
+
     # with open("data.json", "w") as outfile:
     #     json.dump(response_json, outfile)
 
 
     # Look for stash with account name, else keep requesting stashes to look in
     while not found:
+
+        # Check whether we've visted an ID we need to continue from
+        if (os.path.exists("last_visited_id.txt")) and id is 0:
+            with open("last_visited_id.txt", "r") as infile:
+                id = infile.readline()
+
         id_url = f"?id={id}"
         print(f"Requesting url: {api_url}{id_url}")
         response = requests.get(api_url + id_url)
         print(f"Status: {response.status_code}")
+
+        # Write last visited ID to a file
+        with open("last_visited_id.txt", "w") as out:
+            out.write(f"{id}")
 
         response_json = response.json()
 
@@ -34,8 +46,13 @@ def find_tab():
                 if account_name:
                     print(f"Looking in: {account_name}")
                 if (account_name == account):
-                    found = True
                     print("FOUND sporring2")
+
+                    with open("found_at_id.txt", "w") as out:
+                        print("Writing location to a file")
+                        out.write(f"{id}")
+
+                    found = True
 
         if not found:
             id = response_json.get("next_change_id")
